2b12d2e2959cd42d74dd1edb0e558e1f4bac712b
DungNV <dungnv.512@gmail.com>
2022-06-17 09:06:43 +0700

    rebuild

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,8 @@
-export const ENDPOINT_URL = process.env.ENDPOINT_URL || "http://localhost:3000/"
+const API_URL = {
+  development: 'http://localhost:3000/',
+  production: 'https://funny-movies-test-server.herokuapp.com/'
+}
+
+const currentEnv = process.env.NODE_ENV || 'local'
+
+export const ENDPOINT_URL = API_URL[currentEnv]
