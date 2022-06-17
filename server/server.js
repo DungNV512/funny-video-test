3a99a500e1a2046024a178cdfab17d6da3063803
DungNV <nguyenvietdung@gapo.com.vn>
2022-06-17 07:41:57 +0700

    support cors

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const cors = require("cors");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
+server.use(cors());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-server.get('/api', (req, res) => {
-    res.status(200).json({api: 'version 1'})
-  })
+server.get("/api", (req, res) => {
+  res.status(200).json({ api: "version 1" });
+});
 
 server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+  console.log(`JSON Server is running on port ${port}`);
+});
